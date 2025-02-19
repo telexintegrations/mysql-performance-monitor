39c5590c6f0c75016e6f1c9b42c0053af1ab6d4a
dohoudaniel <dohoudanielfavour@gmail.com>
2025-02-19 15:55:26 +0100

    fix: change running port

--- app/main.py
+++ app/main.py
@@ -369,4 +369,4 @@ if __name__ == "__main__":
     """ Run the FastAPI application """
     import uvicorn
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
